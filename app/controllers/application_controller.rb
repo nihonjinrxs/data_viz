55162d2056c84346d6b170a19caebf466c77928f
nihonjinrxs <ryan.b.harvey@gmail.com>
2011-04-22 21:13:20 -0400

    Initial commit.

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,31 @@
+require 'googlevisualr'
+
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+end
+
+def pie_chart
+  # Create a PieChart visualization
+  @chart = GoogleVisualr::PieChart.new
+  
+  # Add Column Headers
+  @chart.add_column('string', 'Task')
+  @chart.add_column('number', 'Hours per Day')
+  
+  # Add Number of Rows and Row Values
+  @chart.add_rows([
+    ['Work'     , 11],
+    ['Eat'      ,  2],
+    ['Commute'  ,  2],
+    ['Watch TV' ,  2],
+    ['Sleep'    ,  7]
+  ]);
+  
+  # Set Configuration Options for PieChart
+  options = { :width => 400, :height => 240,
+              :title => 'My Daily Activities', :is3D => true }
+  options.each_pair do | key, value |
+    @chart.send "#{key}=", value
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DataViz::Application.config.secret_token = '121d48bb799feb93278aba0f75219554adb12c74a9a689afd1877dd3d08d3984c04e466b088db7a54cd821359f6f31d57f3d6e5e0eddb47132675a2b5d049807'
